2409fc76481e7d0365b51c3ad5de7d44b347b5a4
Evan Spiker <umdoobby@gmail.com>
2021-05-01 17:55:11 -0400

    added to tme controller

--- PlzOpenMe/Controllers/TmeController.cs
+++ PlzOpenMe/Controllers/TmeController.cs
@@ -44,9 +44,7 @@ namespace PlzOpenMe.Controllers
             
             // convert the body into the update object
             Update update = JsonSerializer.JsonConvert.DeserializeObject<Update>(Encoding.UTF8.GetString(ms.ToArray()));
-            
-            Log.Debug(Encoding.UTF8.GetString(ms.ToArray()));
-            
+
             // pull out that user and the message
             User updateFrom = update.Message.From;
             Message updateMessage = update.Message;
@@ -54,7 +52,7 @@ namespace PlzOpenMe.Controllers
             // see if we are dealing with a bot
             if (updateFrom.IsBot)
             {
-                _bot.SendTextMessageAsync(updateMessage.Chat.Id, "I'm sorry my bot brethren but I cannot help you. I can only help humans.");
+                _bot.SendTextMessageAsync(updateMessage.Chat.Id, "I'm sorry but I can't help other bots.");
                 return Json(true);
             }
 
@@ -69,27 +67,77 @@ namespace PlzOpenMe.Controllers
             // see if we have the user
             if (fromUser == null)
             {
-                // set up the new user
-                fromUser = new PomUser()
-                {
-                    CreatedOn = DateTime.Now,
-                    Agreed = null,
-                    AgreedOn = null,
-                    UserId = updateFrom.Id
-                };
-
-                // save the new user
-                _dbContext.PomUsers.Add(fromUser);
-                _dbContext.SaveChanges();
+                // so we don't know this user, they only have a few options
                 
-                // send the reply
-                _bot.SendTextMessageAsync(updateMessage.Chat.Id, "I'm sorry my bot brethren but I cannot help you. I can only help humans.");
+                // if they aren't sending me a command that they are allowed to say right now then we will tell them what they can do
+                if (updateMessage.Entities == null)
+                {
+                    _bot.SendTextMessageAsync(updateMessage.Chat.Id, "Hi stranger! I can help you share media and files with people outside of Telegram.\nTo get started say /start or you can say /help for what you can do right now.");
+                    return Json(true);
+                }
+
+                // if they are asking for help we need to tell them what they can do
+                if (updateMessage.Text.StartsWith("/help"))
+                {
+                    _bot.SendTextMessageAsync(updateMessage.Chat.Id, "Say /start to get me ready to start receiving your files.\nSay /info to learn more about me.\nFinally, say /help to have this repeated.");
+                    return Json(true);
+                }
+                
+                // if they want info, lets tell them about us
+                if (updateMessage.Text.StartsWith("/info"))
+                {
+                    _bot.SendTextMessageAsync(updateMessage.Chat.Id, "Let me introduce myself, I am PlzOpenMe, a file and media sharing platform integrated with Telegram. I will take any files you send me, repack them for anyone to see, and give you a link that you can use to share with your friends outside of Telegram. For more information please go to PlzOpen.Me.");
+                    return Json(true);
+                }
+                
+                // if they want to start the service
+                if (updateMessage.Text.StartsWith("/start"))
+                {
+                    // set up the new user
+                    try
+                    {
+                        fromUser = new PomUser()
+                        {
+                            CreatedOn = DateTime.Now,
+                            Agreed = null,
+                            AgreedOn = null,
+                            UserId = updateFrom.Id
+                        };
+
+                        // save the new user
+                        _dbContext.PomUsers.Add(fromUser);
+                        _dbContext.SaveChanges();
+                    }
+                    catch (Exception e)
+                    {
+                        // we failed to set up the user for some reason
+                        _bot.SendTextMessageAsync(updateMessage.Chat.Id, "This is embarrassing... I encountered an error trying to handle your request. I have alerted by sysadmin about the failure, please try again in a few minutes.");
+                        Log.Error(e, $"There was an error while trying to create the user for {updateFrom.Id}/{updateFrom.Username}");
+                        return Json(false);
+                    }
+
+                    // respond with the agreement message
+                    _bot.SendTextMessageAsync(updateMessage.Chat.Id, "Wonderful! To start sharing files, I just need to you to do one thing.\n" +
+                                                                     "PlzOpenMe is a file sharing platform and so I need you to understand what you are " +
+                                                                     "allowed to share, what I'm willing to accept, and what I do with your information.\n" +
+                                                                     "Please read these two pages; https://plzopen.me/Home/Privacy and " +
+                                                                     "https://plzopen.me/Home/Terms.\n\n" +
+                                                                     "Respond with /agree to say that you have read, understand, and agree to the PlzOpen.Me " +
+                                                                     "privacy policy and terms of service. If you do not agree, respond with /stop and I will " +
+                                                                     "delete any information I have collected about your Telegram account.");
+                    return Json(true);
+                }
+                
+                // sending anything else we should just tell them that I don't understand
+                _bot.SendTextMessageAsync(updateMessage.Chat.Id, "I'm sorry but I don't understand that right now. You can say /help for what you can do right now " +
+                                                                 "or you can say /start to get started.");
+                return Json(true);
+
+
             }
             
-            
+            _bot.SendTextMessageAsync(updateMessage.Chat.Id, "thats all i know to say! im not finished yet :/");
 
-
-            Log.Information($"result: {update.Id}:{update.Message.Text}");
             return Json(true);
         }
     }

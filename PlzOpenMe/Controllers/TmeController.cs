29e9865f1fb270229dfadf7fbcfef2150ac4ceb8
Umdoobby <umdoobby@gmail.com>
2021-05-24 14:43:30 -0400

    Update TmeController.cs

--- PlzOpenMe/Controllers/TmeController.cs
+++ PlzOpenMe/Controllers/TmeController.cs
@@ -166,6 +166,81 @@ namespace PlzOpenMe.Controllers
                 // are you sending me a command
                 if (updateMessage.Entities == null)
                 {
+                    // see if they agreed
+                    if (updateMessage.Text.StartsWith("I agree"))
+                    {
+                        try
+                        {
+                            // add the agreement info to their account and update them
+                            fromUser.Agreed = true;
+                            fromUser.AgreedOn = DateTime.Now;
+                            _dbContext.Update(fromUser);
+                            int agreeUserResult = _dbContext.SaveChanges();
+
+                            // log success
+                            Log.Information(
+                                $"The user {updateFrom.Id}/{updateFrom.Username} and agreed and their account has been updated with result {agreeUserResult}");
+
+                            // respond with success
+                            _bot.SendTextMessageAsync(updateMessage.Chat.Id,
+                                "Awesome! Alright, with that out of the way your officially all set up and I'm ready to help! " +
+                                "All you have to do is send me a message with some kind of media attached to it and I will automatically " +
+                                "set it up for you to share. When I'm done packaging it, I will respond with a URL that you can share with " +
+                                "anyone, over any messaging platform. It doesn't matter if you are forwarding me a message from a group, " +
+                                "another bot, a direct message, or you sent me it directly. I will always try to grab that file, repack it " +
+                                "and give you a sharable URL.\n\n" +
+                                "Also if you are interested in my development, how I work, or just generally curious; I'm open source! " +
+                                "You can see issues, upcoming features, recommend new features, report bugs, etc at my GitHub repository!\n" +
+                                "https://github.com/umdoobby/plzopenme \n" +
+                                "If you really like me, maybe give me a star and/or a watch on there as well? No pressure though.\n\n" +
+                                "Finally, you might want to consider joining my update channel.\n" +
+                                "https://t.me/pomUpdates\n" +
+                                "That's where I announce new features, major issues, etc. In case you want to hear about the latest " +
+                                "in protogen packaging technology!... Or if I crash or something I guess... That's all I have to share, " +
+                                "thanks again for giving me a shot!");
+                            return Json(true);
+                        }
+                        catch (Exception e)
+                        {
+                            // for some reason we failed update the user
+                            _bot.SendTextMessageAsync(updateMessage.Chat.Id,
+                                "This is embarrassing... I encountered an error trying to handle your request. I have alerted by sysadmin about the failure, please try again in a few minutes.");
+                            Log.Error(e,
+                                $"There was an error while attempting to save the agreement details of user {updateFrom.Id}/{updateFrom.Username}");
+                            return Json(false);
+                        }
+                    }
+                    
+                    // see if the user wanted to stop
+                    if (updateMessage.Text.StartsWith("Stop"))
+                    {
+                        try
+                        {
+                            // remove them
+                            _dbContext.Remove(fromUser);
+                            int delUserResult = _dbContext.SaveChanges();
+
+                            // log success
+                            Log.Information(
+                                $"The user {updateFrom.Id}/{updateFrom.Username} requested to stop the bot and has been removed from the database with result code {delUserResult}");
+
+                            // respond with success
+                            _bot.SendTextMessageAsync(updateMessage.Chat.Id,
+                                "I'm sorry I couldn't be more help. Your Telegram information has been removed from my database.");
+                            return Json(true);
+                        }
+                        catch (Exception e)
+                        {
+                            // for some reason we failed to remove the user
+                            _bot.SendTextMessageAsync(updateMessage.Chat.Id,
+                                "This is embarrassing... I encountered an error trying to handle your request. I have alerted by sysadmin about the failure, please try again in a few minutes.");
+                            Log.Error(e,
+                                $"There was an error while attempting to remove user {updateFrom.Id}/{updateFrom.Username}");
+                            return Json(false);
+                        }
+                    }
+                    
+                    // anything else at this stage is not acceptable
                     _bot.SendTextMessageAsync(updateMessage.Chat.Id,
                         "I'm sorry but I can only continue to help if you agree to my terms of service and " +
                         "my privacy policy. I'm not a lawyer so they aren't long or complicated, I promise! You can say " +
@@ -213,80 +288,6 @@ namespace PlzOpenMe.Controllers
                         "/start welcome message. Other then that, say /info to learn more about me or say " +
                         "/help to have this repeated.");
                     return Json(true);
-                }
-
-                // so the user doesn't want our help? fine we will remove them
-                if (updateMessage.Text.StartsWith("/stop"))
-                {
-                    try
-                    {
-                        // remove them
-                        _dbContext.Remove(fromUser);
-                        int delUserResult = _dbContext.SaveChanges();
-
-                        // log success
-                        Log.Information(
-                            $"The user {updateFrom.Id}/{updateFrom.Username} requested to stop the bot and has been removed from the database with result code {delUserResult}");
-
-                        // respond with success
-                        _bot.SendTextMessageAsync(updateMessage.Chat.Id,
-                            "I'm sorry I couldn't be more help. Your Telegram information has been removed from my database.");
-                        return Json(true);
-                    }
-                    catch (Exception e)
-                    {
-                        // for some reason we failed to remove the user
-                        _bot.SendTextMessageAsync(updateMessage.Chat.Id,
-                            "This is embarrassing... I encountered an error trying to handle your request. I have alerted by sysadmin about the failure, please try again in a few minutes.");
-                        Log.Error(e,
-                            $"There was an error while attempting to remove user {updateFrom.Id}/{updateFrom.Username}");
-                        return Json(false);
-                    }
-                }
-
-                // they agreed! go ahead and set up their account for use
-                if (updateMessage.Text.StartsWith("/agree"))
-                {
-                    try
-                    {
-                        // add the agreement info to their account and update them
-                        fromUser.Agreed = true;
-                        fromUser.AgreedOn = DateTime.Now;
-                        _dbContext.Update(fromUser);
-                        int agreeUserResult = _dbContext.SaveChanges();
-
-                        // log success
-                        Log.Information(
-                            $"The user {updateFrom.Id}/{updateFrom.Username} and agreed and their account has been updated with result {agreeUserResult}");
-
-                        // respond with success
-                        _bot.SendTextMessageAsync(updateMessage.Chat.Id,
-                            "Awesome! Alright, with that out of the way your officially all set up and I'm ready to help! " +
-                            "All you have to do is send me a message with some kind of media attached to it and I will automatically " +
-                            "set it up for you to share. When I'm done packaging it, I will respond with a URL that you can share with " +
-                            "anyone, over any messaging platform. It doesn't matter if you are forwarding me a message from a group, " +
-                            "another bot, a direct message, or you sent me it directly. I will always try to grab that file, repack it " +
-                            "and give you a sharable URL.\n\n" +
-                            "Also if you are interested in my development, how I work, or just generally curious; I'm open source! " +
-                            "You can see issues, upcoming features, recommend new features, report bugs, etc at my GitHub repository!\n" +
-                            "https://github.com/umdoobby/plzopenme \n" +
-                            "If you really like me, maybe give me a star and/or a watch on there as well? No pressure though.\n\n" +
-                            "Finally, you might want to consider joining my update channel.\n" +
-                            "https://t.me/pomUpdates\n" +
-                            "That's where I announce new features, major issues, etc. In case you want to hear about the latest " +
-                            "in protogen packaging technology!... Or if I crash or something I guess... That's all I have to share, " +
-                            "thanks again for giving me a shot!");
-                        return Json(true);
-                    }
-                    catch (Exception e)
-                    {
-                        // for some reason we failed update the user
-                        _bot.SendTextMessageAsync(updateMessage.Chat.Id,
-                            "This is embarrassing... I encountered an error trying to handle your request. I have alerted by sysadmin about the failure, please try again in a few minutes.");
-                        Log.Error(e,
-                            $"There was an error while attempting to save the agreement details of user {updateFrom.Id}/{updateFrom.Username}");
-                        return Json(false);
-                    }
                 }
 
                 // sending anything else we should just tell them that I don't understand
